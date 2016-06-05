27e12ada491b59dc207310ffea13d72de46a8b46
maxwong <maxwong@live.cn>
2016-06-05 22:32:02 +0800

    1. Added add.html to add materials and plans.

---
+++ static/js/add.js
@@ -0,0 +1,18 @@
+/**
+ * Created by maxwong on 6/5/16.
+ */
+
+
+function add_section() {
+    var section_root = $("#section");
+
+    var sections = $("#section > div");
+
+    
+
+    console.log("add_section");
+}
+
+function submit() {
+    console.log("submit");
+}
